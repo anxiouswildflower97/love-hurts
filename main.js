b2b0c7aaa5982820ba9b072479781eb5ef290a03
Carson Snow <anxiouswildflower97@gmail.com>
2023-05-09 22:03:58 -0400

    love-hurts

---
+++ main.js
@@ -0,0 +1,119 @@
+function myFunction1() {
+var x = document.getElementById("q1");
+if (x.innerHTML === "Whenever I look back and wonder, why was he the one?, I land on this: he smiled. We were strangers in the same room at a student conference, and I was staring at him, and he caught my stare, and he welcomed it. Had he been blank-faced or scowling or even intensely cruising, I would have shied away, but his smile invited me in. It said, let’s start something. That was all I needed. My strong, gentle, older-brother-savior had arrived.") {
+    x.innerHTML = "I first met AL in the fall of 2020. At the time, my office was the old teacher’s lounge room. There were a few things that stood out right away. His colorful lunchboxes was the first thing. He would walk in every morning to put his lunchbox in the fridge - somedays it was a Mario and Luigi lunchbox, others a green Mutant Ninja Turtle one, and others a Spiderman lunchbox. The second thing that stood out was his style, it was unique and colorful. The third thing that stood out was how friendly he was. I was nervous after my experience at my first school, so a friendly face was welcomed. <br><br> On the last day of the school year, I was packing my bag up to go home. I was excited about summer break. It was a weird school year. We went remote from November 2020 to March 2021. Then return to hybrid learning. He came up to me and asked me if I was going to the neighborhood pub after work for the end of year staff get together. I didn’t realize that was an event. He said I should come along and join them. I wasn’t really much of a social person, but figured after all the isolation from COVID, it might be a good idea to go out and meet new people. I learned he was going to the West Coast for the summer and he told me to hit him up when he comes back. <br><br> And with that, summer break began. But, it was not a chill summer.";
+
+    x.style.fontFamily = 'Caveat';
+    x.style.fontSize = "large";
+} else {
+    x.innerHTML = "Whenever I look back and wonder, why was he the one?, I land on this: he smiled. We were strangers in the same room at a student conference, and I was staring at him, and he caught my stare, and he welcomed it. Had he been blank-faced or scowling or even intensely cruising, I would have shied away, but his smile invited me in. It said, let’s start something. That was all I needed. My strong, gentle, older-brother-savior had arrived.";
+    
+    x.style.fontFamily = 'Syne Mono';
+    x.style.fontSize = "medium";
+}
+}
+
+function myFunction2() {
+var x = document.getElementById("q2");
+    if (x.innerHTML === "The call wakes me from a deep sleep. My father’s voice: “She’s gone.” The sky is shading from violet night to pale morning blue as we walk toward 14th Street to the lot where Derek has started parking the car he used to keep at Marilyn’s. Voices grow louder and laughter creeps into conversations, a welcome life to the dreary mood but also, for me, the unwelcome sign that life is already moving on.") {
+        x.innerHTML = "During the summer of 2021, a close family friend had suddenly passed away. My mom’s best friend’s husband died. His son texted my mother. It was surreal. It was July 30th, 2021. The time was around 7:30 in the morning. It was such a beautiful day. The sky was blue, not a cloud in sight. It was a breezy day, a welcome relief from the summer heat. He had went to the restroom and never came out. They found him lying on the floor. It was so sudden. It was the first death I experienced as a conscious adult aware of what death is. The only other death I experienced was the of my younger brother when I was seven. But, I don’t remember anything about it since he died a few days after his birth. <br><br> We spent the rest of August supporting my mom’s best friend. She’s like a second mom, she’s seen me grow up. For a while, I was worried if she would ever be the same again. <br><br> Something that I thought about a lot during this time was when people die, we say to live everyday like it’s your last. Do what you want to do, do things that make you happy, because you can die tomorrow. But, once everything is said and done, we go back to worrying, doing things we don’t want to, pushing things we want to do to later as if we have all of eternity. I always thought that was strange, how quickly we forget we’re not immortal.";
+
+        x.style.fontFamily = 'Caveat';
+        x.style.fontSize = "large";
+    } else {
+        x.innerHTML = "The call wakes me from a deep sleep. My father’s voice: “She’s gone.” The sky is shading from violet night to pale morning blue as we walk toward 14th Street to the lot where Derek has started parking the car he used to keep at Marilyn’s. Voices grow louder and laughter creeps into conversations, a welcome life to the dreary mood but also, for me, the unwelcome sign that life is already moving on.";
+        
+        x.style.fontFamily = 'Syne Mono';
+        x.style.fontSize = "medium";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("q3");
+        if (x.innerHTML === "Zack had seemed like my special discovery, but the opposite is true. His appeal is obvious to anyone. He’s one of those hot guys I swore I’d never fall for. I was already telling myself stories about Zack. I still am.") {
+            x.innerHTML = "In August of 2021, I reached out to AL. I asked him how his trip was going and whether he made it up to Seattle like he was planning to. He was excited to hear from me. He suggested we meet up when he comes back. On September 1st, we met up for lunch. I don’t know if that was when I first realized I had feelings for him. But it was on this day when I realized how handsome he was. He told me about his trip, his side hustle selling vintage clothing, and his break-up with his girlfriend. <br><br> I didn’t really know what to say. I don’t travel a lot. I don’t hustle a lot. I never dated before, so I had no idea what the right words are to say to someone who has experienced a break-up. But, it didn’t really matter that I didn’t have much to say. I could listen to him talk all day, he had so much to say. He was smart, he travelled to lots of cool places, he was creative. He was a great conversationalist.";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "Zack had seemed like my special discovery, but the opposite is true. His appeal is obvious to anyone. He’s one of those hot guys I swore I’d never fall for. I was already telling myself stories about Zack. I still am.";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
+
+function myFunction4() {
+    var x = document.getElementById("q4");
+        if (x.innerHTML === "We die, they do nothing is not just a chant, it’s the truth behind all those horror stories about people with AIDS lying on gurneys in the corridors of hospitals waiting for rooms, ignored.") {
+            x.innerHTML = "I worked at the REC Center during the summer of 2020. I remember feeling terrified of bringing COVID home to my immunocompromised parents. We all were sick with COVID during the spring of 2020 and stayed sick for weeks. We didn’t go to the hospital because we worried that we would die alone. If we died, it would be better if died in together in our home, in each other’s company. Thankfully, we recovered. However, I was worried about bringing the virus home again. <br><br> That worry carried over as schools reopened for hybrid learning in September 2020. My worries were lessened when I realized we would have 6 students in the classroom opposed to the 32 students we typically have. I was more relieved when schools went remote from November 2020 to March 2021 so that the chances of getting sick and bringing it home declined tremendously. <br><br> However, my worry turned into frustration when I learned that in September 2021, we would be returning to full class sizes of 32 students. We would still be required to wear masks (don’t have to tell me twice). However, we had to arrange the desks to ensure there was a distance of at least a few feet between students. My classroom was small, the most room I could create between desks was a foot. In addition to returning to normal class sizes, we returned to normal curriculum and instruction. My school believed heavily in testing and we went back to giving five assessments, one project, and two quizzes per unit. We completely ignored the fact that students needed time to adjust to returning to school. Students haven’t been with their peers for almost a year and a half. They haven’t taken tests in a traditional manner for a year and a half. They have seen the world turn upside down, and now we expect them to just adjust as we turn in right side up without any scaffolding or support? Schools preached that we would emphasize social-emotional learning and incorporate that into school culture to support students as we return to normal. We didn’t. Instead, we welcomed them with loads of testing, and the pressure to catch-up and not fall behind because we were already so behind.";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "We die, they do nothing is not just a chant, it’s the truth behind all those horror stories about people with AIDS lying on gurneys in the corridors of hospitals waiting for rooms, ignored.";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
+
+function myFunction5() {
+    var x = document.getElementById("q5");
+        if (x.innerHTML === "I sense someone’s eyes on me - and sure enough, there’s Zack across the apartment, looking our way. I offer him a little wave, and he blows me a kiss.") {
+            x.innerHTML = "Every morning, AL would walk by my classroom. We would talk until the first bell would ring and then get into our workday. Sometimes when I was teaching my lesson, I would hear the students laugh. When I’d turn around to see why, I would see he was sitting in one of the desks, waiting for me too notice. He started inviting me to hang out with him outside of work. First, it was the beginning of the year staff party that he arranged with a few other teachers at a bar in Astoria. Then, it was to a Halloween Party his friend was holding at their place in Brooklyn. Then, it was for sushi with his friends at a place in Elmhurst. <br><br> We started to grow closer as our friendship developed. Growing up, I was pretty shy. In college, I was so busy with school and work, that my social life wasn’t much of a priority. And then COVID came along and I lost touch with the few people I did speak with during undergrad. So, I was excited to finally be making friends again. And I was definitely grateful to have met someone as fun and as nice as AL.";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "I sense someone’s eyes on me - and sure enough, there’s Zack across the apartment, looking our way. I offer him a little wave, and he blows me a kiss.";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
+
+function myFunction6() {
+    var x = document.getElementById("q6");
+        if (x.innerHTML === "We became friends. It seemed unlikely to me. I’d never been friends with anyone who looked like him. But we had more in common that you’d think. He was not a trophy to me. He was someone I loved.") {
+            x.innerHTML = "On the night we went out for sushi, I remember I had parked my car far away from the restaurant. I had trouble finding a parking spot that didn’t require parallel parking. I ended up parking a 15 minute walk away. We started wrapping up dinner at around 10ish and I wasn’t feeling confident walking alone to the backstreets in the dark. AL walked me back to my car, and then I drove him back to the restaurant so he could get back to his motorcycle. I remember feeling so many butterflies on the walk back to my car. I remember thinking why in the hell would someone as cool as him want to be friends with me? To this day, I still wonder why. <br><br> That was the night I recognized that I had feelings for him. It was the way he laughed like a child, such an innocent and goofy laugh. It was the way his cheeks and ears turned red when he would drink alcohol. It was the way his eyes would light up as he talked to everyone, truly listening to every word they said. I was terrified. I was confident he didn’t have feelings for me. And I didn’t want to ruin our friendship.";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "We became friends. It seemed unlikely to me. I’d never been friends with anyone who looked like him. But we had more in common that you’d think. He was not a trophy to me. He was someone I loved.";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
+
+function myFunction7() {
+    var x = document.getElementById("q7");
+        if (x.innerHTML === "But what if you’re consumed by anger about your own state of being? How do you protest that?") {
+            x.innerHTML = "Around the winter of 2021, I started spiraling downwards. I was feeling burnt out. This was my third year in the profession and my feelings about it still remain the same. I want to love it, but I don’t. It was the truth I didn’t want to face. All I’ve ever dreamed about since I was five years old was to be a teacher. And now I was watching my dream fall apart right before me. I was feeling like a failure for dropping out of my master’s program mid-semester. I already felt so far behind because I was now entering my third gap year. My self-esteem plummeted due to the weight I had gained from binge eating. There were so many other things as well, but I’d rather not get into them. As each day went by, I was feeling stuck in my life. I was feeling more and more disgusted by myself. ";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "But what if you’re consumed by anger about your own state of being? How do you protest that?";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
+
+function myFunction8() {
+    var x = document.getElementById("q8");
+        if (x.innerHTML === "He says, “You must be so sad,” a simple and obvious sentiment but one I haven’t yet voiced. “It’s okay to let it out,” he says, which is all it takes, and then I’m crying the tears I haven’t yet shed, while he holds me and rocks me in his arms.") {
+            x.innerHTML = "AL noticed my arms one day. I tried to deny it, but he wouldn’t let up. I couldn’t find any words and started crying instead. In that moment, I felt embarrassed that he saw, scared about what would happen next, and relieved that I wasn’t alone in this anymore. That hug felt like a momentary escape from everything that I was struggling with. I felt safe. Two weeks later, he and I are at a therapists office. He found a therapist and he came along with me the first sessions. <br><br> I’m so grateful that he did that. That therapist saved me. The only reason I am in the position I am in today is because of her. She helped me get through the school year in one piece, and helped me through change, something that I don’t really enjoy. I don’t even want to imagine what my life would have been like if he didn’t intervene when he did. ";
+    
+            x.style.fontFamily = 'Caveat';
+            x.style.fontSize = "large";
+        } else {
+            x.innerHTML = "He says, “You must be so sad,” a simple and obvious sentiment but one I haven’t yet voiced. “It’s okay to let it out,” he says, which is all it takes, and then I’m crying the tears I haven’t yet shed, while he holds me and rocks me in his arms.";
+            
+            x.style.fontFamily = 'Syne Mono';
+            x.style.fontSize = "medium";
+        }
+    }
